6fadbc14042ed54ed179be0229324d7fabb53802
Logan <ljordan@cocc.edu>
2024-05-21 17:27:05 -0700

    Add questions

---
+++ script.js
@@ -0,0 +1,48 @@
+const questions = [
+    {
+        question: "What is the tiny piece at the end of a shoelace called?",
+        answers: [
+            {text: "Aglet", correct: true},
+            {text: "End", correct: false},
+            {text: "Tie", correct: false},
+            {text: "Thingy", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following languages has the longest alphabet?",
+        answers: [
+            {text: "Arabic", correct: false},
+            {text: "English", correct: false},
+            {text: "Greek", correct: false},
+            {text: "Russian", correct: true},
+        ]
+    },
+    {
+        question: "Which of the following is NOT a fruit?",
+        answers: [
+            {text: "Tomatoes", correct: false},
+            {text: "Rhubarb", correct: true},
+            {text: "Avocados", correct: false},
+            {text: "Pumpkins", correct: false},
+        ]
+    },
+    {
+        question: "What city hosted the 2002 Olympic Games?",
+        answers: [
+            {text: "Tokyo", correct: false},
+            {text: "Los Angeles", correct: false},
+            {text: "Sydney", correct: true},
+            {text: "Oslo", correct: false},
+        ]
+    },
+    {
+        question: "What color is the sunset on Mars?",
+        answers: [
+            {text: "Blue", correct: true},
+            {text: "Green", correct: false},
+            {text: "Red", correct: false},
+            {text: "Pink", correct: false},
+        ]
+    }
+    
+]
