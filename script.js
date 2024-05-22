5c7a04bc4f6070e1d9548aba0f3a4172eb5cd1fd
Logan <ljordan@cocc.edu>
2024-05-22 16:46:07 -0700

    Add more questions and random answer positions

--- script.js
+++ script.js
@@ -5,34 +5,34 @@ const questions = [
             {text: "Aglet", correct: true},
             {text: "End", correct: false},
             {text: "Tie", correct: false},
-            {text: "Thingy", correct: false},
+            {text: "Thingy", correct: false}
         ]
     },
     {
         question: "Which of the following languages has the longest alphabet?",
         answers: [
+            {text: "Russian", correct: true},
             {text: "Arabic", correct: false},
             {text: "English", correct: false},
-            {text: "Greek", correct: false},
-            {text: "Russian", correct: true},
+            {text: "Greek", correct: false}
         ]
     },
     {
         question: "Which of the following is NOT a fruit?",
         answers: [
-            {text: "Tomatoes", correct: false},
             {text: "Rhubarb", correct: true},
+            {text: "Tomatoes", correct: false},
             {text: "Avocados", correct: false},
-            {text: "Pumpkins", correct: false},
+            {text: "Pumpkins", correct: false}
         ]
     },
     {
         question: "What city hosted the 2002 Olympic Games?",
         answers: [
+            {text: "Sydney", correct: true},
             {text: "Tokyo", correct: false},
             {text: "Los Angeles", correct: false},
-            {text: "Sydney", correct: true},
-            {text: "Oslo", correct: false},
+            {text: "Oslo", correct: false}
         ]
     },
     {
@@ -41,10 +41,55 @@ const questions = [
             {text: "Blue", correct: true},
             {text: "Green", correct: false},
             {text: "Red", correct: false},
-            {text: "Pink", correct: false},
+            {text: "Pink", correct: false}
+        ]
+    },
+    {
+        question: "How many bones are in the human body?",
+        answers: [
+            {text: "206", correct: true},
+            {text: "300", correct: false},
+            {text: "195", correct: false},
+            {text: "777", correct: false}
+        ]
+    },
+    {
+        question: "Which U.S. state is the only state to grow coffee beans?",
+        answers: [
+            {text: "Hawaii", correct: true},
+            {text: "Oregon", correct: false},
+            {text: "Wyoming", correct: false},
+            {text: "Florida", correct: false}
+        ]
+    },
+    {
+        question: "What is the tallest breed of dog?",
+        answers: [
+            {text: "Great Dane", correct: true},
+            {text: "German Shepard", correct: false},
+            {text: "Chihuahua", correct: false},
+            {text: "Beagle", correct: false}
+        ]
+    },
+    {
+        question: "What is the largest continent?",
+        answers: [
+            {text: "Asia", correct: true},
+            {text: "North America", correct: false},
+            {text: "Europe", correct: false},
+            {text: "Africa", correct: false}
+        ]
+    },
+    {
+        question: "In what year was the internet opened to the public?",
+        answers: [
+            {text: "1993", correct: true},
+            {text: "1990", correct: false},
+            {text: "2000", correct: false},
+            {text: "2024", correct: false}
         ]
     }
-];
+]
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
@@ -65,7 +110,7 @@ function showQuestion(){
     let currentQuestion = questions[currentQuestionIndex];
     let questionNum = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNum + ". " + currentQuestion.question;
-    currentQuestion.answers.forEach(answer => {
+    currentQuestion.answers.sort( () => Math.random()-0.5 ).forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
